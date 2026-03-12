1a5994474f5ab7cf274982f5a82e385074943c9c
shuya729 <sshuya29@gmail.com>
2026-03-12 14:52:48 +0900

    schema のDB名を変更

--- apps/mobile/src/lib/db/schema.ts
+++ apps/mobile/src/lib/db/schema.ts
@@ -1,8 +1,14 @@
 import { sql } from "drizzle-orm";
-import { check, int, integer, sqliteTable } from "drizzle-orm/sqlite-core";
+import {
+  check,
+  index,
+  int,
+  integer,
+  sqliteTable,
+} from "drizzle-orm/sqlite-core";
 
 export const events = sqliteTable(
-  "events",
+  "archives",
   {
     id: int("id").primaryKey(),
     startAt: integer("start_at", { mode: "timestamp" })
@@ -14,6 +20,8 @@ export const events = sqliteTable(
   },
   (table) => [
     check("start_at_before_end_at", sql`${table.startAt} <= ${table.endAt}`),
+    index("archives_start_at_idx").on(table.startAt),
+    index("archives_end_at_idx").on(table.endAt),
   ]
 );
 
