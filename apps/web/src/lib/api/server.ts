c4d6b31efad9892360e1fd1525ff3702f483434c
shuya729 <sshuya29@gmail.com>
2026-05-05 21:48:11 +0900

    webをinit

---
+++ apps/web/src/lib/api/server.ts
@@ -0,0 +1,7 @@
+import createFetchClient from "openapi-fetch";
+import type { paths } from "./paths";
+
+export const serverApi = createFetchClient<paths>({
+  baseUrl: process.env.NEXT_PUBLIC_API_URL,
+  credentials: "include",
+});
